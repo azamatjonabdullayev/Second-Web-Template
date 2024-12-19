775571a0ab256bec8dd6853948c39fb7b2a28640
Azamat <abdullayevazamat2705@gmail.com>
2024-12-19 17:18:49 +0500

    Added several paths to .html files

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/service')
+def service():
+    return render_template("service.html")
+
+@app.route('/team')
+def team():
+    return render_template("team.html")
+
+@app.route('/why')
+def why():
+    return render_template("why.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
